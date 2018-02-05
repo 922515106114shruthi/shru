ce3416cff9a1ca4183d11b735ad896ff19f96d96
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-05 16:55:54 +0530

    Create digit.C

---
+++ digit.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#includE<conio.h>
+void main()
+{
+ int n=3,i,a[20];
+  for(i=1;i<=n;i++)
+  {
+  	scanf("%d",&a[i]);
+  }
+  for(i=1;i<=n;i++)
+  {
+  	printf(" %d",a[i]);
+  }
+  getch();
+  }
