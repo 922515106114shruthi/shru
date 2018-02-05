ab533f12d93f78d5389961a58f703dbdff505ccf
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-05 16:46:11 +0530

    Update index.C

--- index.C
+++ index.C
@@ -5,11 +5,11 @@ void main()
 int i,a[20],n;
 printf(" enter the range:");
 scanf("%d",&n);
-  for(i=1;i<=n;i++)
+  for(i=0;i<=n;i++)
   {
   	scanf("%d",&a[i]);
   }
-  for(i=1;i<=n;i++)
+  for(i=0;i<=n;i++)
   {
   	printf("%d %d\n",a[i],i);
   
